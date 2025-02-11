cd6e1d477bc58a832bf6929d1c22cd36d48c915b
Erlaitz <erlaitz.alonso05@somo.eus>
2025-02-11 12:54:06 +0100

    Biblioteca de muskiz directory

--- Biblioteca de muskiz/scripts.js
+++
@@ -1,5 +0,0 @@
-/* ver el texto que tiene cada libo al hacer click en la foto. en el catálogo y en en el index */
-
-function alert() {
-    alert("Gracias por sus comentarios!!")
-}
--- Biblioteca de muskiz/views/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $SERVIDOR = "localhost";
-    $USUARIO = "root";
-    $CONTRASENA = "";
-    $BASE = "bdwwwreto";
-    $CONEXION = @mysqli_connect($SERVIDOR, $USUARIO, $CONTRASENA, $BASE) or die("Fallo de conexión");
-?>
