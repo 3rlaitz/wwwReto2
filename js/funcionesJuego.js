9a3f2ac5dd4a4d935f8b94967155fbc52530d113
Erlaitz <erlaitz.alonso05@somo.eus>
2025-04-25 12:51:05 +0200

    juego en JavaScript
    creación de un juego en JavaScript que trata de buscar el tesoro escondido en un mapa.

---
+++ js/funcionesJuego.js
@@ -0,0 +1,30 @@
+// Generar un número aleatorio para la posición del tesoro
+let getRandomNumber = size => {
+    return Math.floor(Math.random() * size);
+}
+
+  // Calcular la distancia entre el tesoro y el punto clickado (pitágoras)
+let getDistance = (e, target) => {
+    let diffX = e.offsetX - target.x;
+    let diffY = e.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+}
+
+  // Te dice cómo de cerca o lejos estás del tesoro
+let getDistanceText = distance => {
+    if (distance < 30) {
+        return "Tevas a Quemar!";
+    } else if (distance < 40) {
+        return "Muy Caliente";
+    } else if (distance < 60) {
+        return "Caliente";
+    } else if (distance < 100) {
+        return "Templado";
+    } else if (distance < 180) {
+        return "Frío";
+    } else if (distance < 360) {
+        return "Muy Frío";
+    } else {
+        return "Helado!";
+    }
+}
---
+++ js/juego.js
@@ -0,0 +1,25 @@
+const WIDTH = 400;
+const HEIGH = 400;
+
+let target = {
+    x: getRandomNumber(WIDTH),
+    y: getRandomNumber(HEIGH)
+};
+
+// click handler
+let $map = document.querySelector('#map');
+let $distance = document.querySelector('#distance');
+let intentos = 0;
+
+$map.addEventListener('click', function (e) {
+    console.log('click');
+    intentos++;
+    let distance = getDistance(e, target);
+    let distanceText = getDistanceText(distance);
+    $distance.innerHTML = `<h1>${distanceText}</h1>`;
+
+    if (distance < 20 ) {
+        alert(`Has encontrado el tesoto en  ${intentos} intentos!`);
+        location.reload();
+    }
+});
